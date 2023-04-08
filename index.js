65f94ac0248a02debc67e0c8acc3778d7947855e
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-04-08 19:00:50 +0100

    added footer, image slideshow and made some changes to the google maps embed

---
+++ index.js
@@ -0,0 +1,55 @@
+let slideIndex = 0;
+let timer;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    timer = setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
+
+function changeSlide(n) {
+    clearTimeout(timer);
+    if (n < 0) {
+        showPrevSlide(slideIndex += n);
+    } else {
+        showNextSlide(slideIndex += n);
+    }
+}
+
+function showPrevSlide(n) {
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+function showNextSlide(n) {
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
+showSlides();
+
+
+//Modal on image click//
+
+function openImageModal(imageElement) {
+    const modalImage = document.getElementById('modalImage');
+    modalImage.src = imageElement.src;
+    const imageModal = new bootstrap.Modal(document.getElementById('imageModal'));
+    imageModal.show();
+  }
+  
