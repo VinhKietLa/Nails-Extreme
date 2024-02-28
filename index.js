dd7893d06b0f660cb2f6da34f768435c8238eec4
Vinh La <kietla@live.co.uk>
2024-02-28 22:34:51 +1100

    updating pricing list and some additional style tweaks

--- index.js
+++ index.js
@@ -1,63 +1,16 @@
-let slideIndex = 0;
-let timer;
-
-function showSlides() {
-    let slides = document.getElementsByClassName("slide");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    timer = setTimeout(showSlides, 4000);
+function openImageModal() {
+  const galleryModal = new bootstrap.Modal(
+    document.getElementById("galleryModal")
+  );
+  galleryModal.show();
 }
 
-function changeSlide(n) {
-    clearTimeout(timer);
-    if (n < 0) {
-        showPrevSlide(slideIndex += n);
-    } else {
-        showNextSlide(slideIndex += n);
-    }
-}
-
-function showPrevSlide(n) {
-    let slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
-
-function showNextSlide(n) {
-    let slides = document.getElementsByClassName("slide");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-}
-
-showSlides();
-
-
-//Modal on image click//
-
-function openImageModal(imageElement) {
-    const modalImage = document.getElementById('modalImage');
-    modalImage.src = imageElement.src;
-    const imageModal = new bootstrap.Modal(document.getElementById('imageModal'));
-    imageModal.show();
-  }
-  
-//Scroll up btn//
-document.getElementById('scrollUpButton').addEventListener('click', function () {
+document
+  .getElementById("scrollUpButton")
+  .addEventListener("click", function () {
     window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
+      top: 0,
+      left: 0,
+      behavior: "smooth",
     });
-});
+  });
